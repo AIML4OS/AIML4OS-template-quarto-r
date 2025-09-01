4b7ee685a8a6d1b08632c1e6e5ca04549a6e054b
Olivier Meslin <44379737+oliviermeslin@users.noreply.github.com>
2025-09-01 17:19:24 +0200

    Debug restore_environment.sh

--- sspcloud/restore_environment.sh
+++ sspcloud/restore_environment.sh
@@ -2,4 +2,7 @@
 echo "Executing the restore_environment.sh script"
 
 # Install all dependencies in the system folder
-Rscript -e "setwd('/home/onyxia/work/$MY_REPO')" -e 'renv::restore()'
+Rscript -e "setwd('/home/onyxia/work/$MY_REPO')"
+Rscript -e "getwd()"
+Rscript -e "print('renv.lock' %in% list.files())"
+Rscript -e 'renv::restore()'
