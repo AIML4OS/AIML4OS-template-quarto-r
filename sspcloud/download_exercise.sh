a79c4d799ae75f01e85c3b597a5f22cbb154a1d1
oliviermeslin <olivier.meslin@insee.fr>
2025-09-01 13:05:18 +0000

    Improve init script

--- sspcloud/download_exercise.sh
+++ sspcloud/download_exercise.sh
@@ -4,17 +4,15 @@ echo "Executing the download_exercise.sh script"
 # Retrieve the URL of the website deployed on Github Pages
 cd $MY_REPO
 export GITHUB_OWNER=$(git remote get-url origin | sed -E 's/.*github.com[:\/]([^\/]+)\/.*/\1/')
-export GH_PAGES_WEBSITE="https://${GITHUB_OWNER}.github.io/${MY_REPO}"
-cd ..
+export GITHUB_REPO="https://www.github/${GITHUB_OWNER}/${MY_REPO}"
 
-echo $GH_PAGES_WEBSITE
+echo $GITHUB_REPO
 
 # Define the URL of the exercise
-EXERCISE_DOWNLOAD_URL="${GH_PAGES_WEBSITE}/${EXERCISE_PATH}"
+EXERCISE_DOWNLOAD_URL="${GITHUB_REPO}/${EXERCISE_PATH}"
 
 echo $EXERCISE_DOWNLOAD_URL
 
 # Download the EXERCISE directly using curl
-WORK_DIR="/home/onyxia/work"
 echo $EXERCISE_DOWNLOAD_URL
-curl -L $EXERCISE_DOWNLOAD_URL -o "${WORK_DIR}/exercise.ipynb"
+curl -L $EXERCISE_DOWNLOAD_URL -o "${MY_REPO}/exercise.qmd"
