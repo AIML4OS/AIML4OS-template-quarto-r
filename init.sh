5242c815af8a7e49bbba792ca47b9b4e60d2bdfc
oliviermeslin <olivier.meslin@insee.fr>
2025-09-02 10:07:42 +0000

    Remove environment restoration

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ export MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
 echo /home/onyxia/work/$MY_REPO
 
-# Restore the environment
-sh $MY_REPO/sspcloud/restore_environment.sh
+# Do not restore the environment (you should do it by hand)
+# sh $MY_REPO/sspcloud/restore_environment.sh
 
 # Download data
 sh $MY_REPO/sspcloud/download_data.sh
