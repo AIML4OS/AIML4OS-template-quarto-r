6ce1a1bb657a3ff3d77e67ea7f522508183011ae
Olivier Meslin <44379737+oliviermeslin@users.noreply.github.com>
2025-09-11 12:57:59 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,8 +5,8 @@ export MY_REPO=$(ls -d "/home/onyxia/work"/*/ | head -n 1 | xargs basename)
 
 echo /home/onyxia/work/$MY_REPO
 
-# Do not restore the environment (you should do it by hand)
-# sh $MY_REPO/sspcloud/restore_environment.sh
+# Restore the environment
+sh $MY_REPO/sspcloud/restore_environment.sh
 
 # Download data
 sh $MY_REPO/sspcloud/download_data.sh
