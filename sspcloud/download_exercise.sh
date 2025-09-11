a8da933aeb1fcb26af297702c5c98c88fe768170
oliviermeslin <olivier.meslin@insee.fr>
2025-09-11 08:55:31 +0000

    Change download path to raw path

--- sspcloud/download_exercise.sh
+++ sspcloud/download_exercise.sh
@@ -4,12 +4,12 @@ echo "Executing the download_exercise.sh script"
 # Retrieve the URL of the website deployed on Github Pages
 cd $MY_REPO
 export GITHUB_OWNER=$(git remote get-url origin | sed -E 's/.*github.com[:\/]([^\/]+)\/.*/\1/')
-export GITHUB_REPO="https://www.github/${GITHUB_OWNER}/${MY_REPO}"
+export GITHUB_REPO="https://raw.githubusercontent.com/${GITHUB_OWNER}/${MY_REPO}"
 
 echo $GITHUB_REPO
 
 # Define the URL of the exercise
-EXERCISE_DOWNLOAD_URL="${GITHUB_REPO}/${EXERCISE_PATH}"
+EXERCISE_DOWNLOAD_URL="${GITHUB_REPO}/refs/heads/main/${EXERCISE_PATH}"
 
 echo $EXERCISE_DOWNLOAD_URL
 
